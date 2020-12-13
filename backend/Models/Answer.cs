caddeec8721b130a9d33e228fa0a20a3a3a8792e
Daniel Dantas <daniel.leite.dantas@gmail.com>
2020-12-13 12:37:48 -0300

    Add models for Quiz, Question and Answer

---
+++ backend/Models/Answer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizMania.WebAPI.Models
+{
+    public class Answer
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public bool IsCorrect { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Text { get; set; }
+    }
+}
---
+++ backend/Models/Quiz.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuizMania.WebAPI.Models
+{
+    public class Quiz
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public ICollection<Question> Questions { get; set; }
+    }
+}
