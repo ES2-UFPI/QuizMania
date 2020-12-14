b74f5db5f6f8444f73154b1c92c9070c17fc88a9
Carlos Daniel <carlosd.1199@gmail.com>
2020-12-14 15:34:19 -0300

    Adiciona cardItem como componente #5

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -4,4 +4,5 @@ import Pergunta from "./Pergunta";
 import Alternativa from "./Alternativa";
 import Gabarito from "./Gabarito";
 import ResponsiveList from "./ResponsiveList";
-export { Container, Header, Pergunta, Alternativa, Gabarito, ResponsiveList };
+import CardItem from "./CardItem";
+export { Container, Header, Pergunta, Alternativa, Gabarito, ResponsiveList, CardItem };
--- frontend/src/screens/Gold/Usar/index.js
+++ frontend/src/screens/Gold/Usar/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ScrollView, View, Text, FlatList, Image } from "react-native";
-import { Container, Header, ResponsiveList } from "../../../../components";
+import { Container, Header, ResponsiveList, CardItem } from "../../../../components";
 import { Card, Button } from "react-native-elements";
 
 export default function Usar() {
@@ -14,45 +14,7 @@ export default function Usar() {
           {Array(10)
             .fill(0)
             .map((item, index) => (
-              <Card containerStyle={{ width: 130, }}>
-                <Image
-                  source={require("../../../../assets/images/vidas.png")}
-                  style={{ height: 40, width: 40, alignSelf: "center" }}
-                />
-                <Card.Title>Pack {index}* vidas</Card.Title>
-                <Card.Divider />
-
-                <View
-                  style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <Text
-                    style={{
-                      fontStyle: "italic",
-                      color: "gray",
-                      margin: 0,
-                      fontWeight: "bold",
-                      marginRight: 5,
-                      marginBottom: 5,
-                    }}
-                  >
-                    {index * 600}
-                  </Text>
-                  <Image
-                    source={require("../../../../assets/images/moeda.png")}
-                    style={{ height: 15, width: 15, marginBottom: 5 }}
-                  />
-                </View>
-                <Card.Divider />
-                <Button
-                  title="Obter"
-                  type="clear"
-                  onPress={() => alert("Obtendo item..." + index)}
-                />
-              </Card>
+              <CardItem index={index}/>
             ))}
         </ResponsiveList>
       </ScrollView>
