69306c8359e2793aeb96187b31a1dc78e9be9dd3
Daniel Dantas <daniel.leite.dantas@gmail.com>
2021-01-27 09:40:25 -0300

    Initial setup of backend tests #87

---
+++ QuizMania.WebAPI.Tests/DbContextTestBase.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using QuizMania.WebAPI.Data;
+using Microsoft.Data.Sqlite;
+
+namespace QuizMania.WebAPI.Tests
+{
+    public class DbContextTestBase
+    {
+        private DbContextOptionsBuilder<DatabaseContext> _dbContextOptBuilder;
+        private DatabaseContext _dbContext;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var connection = new SqliteConnection("DataSource = file::memory:?cache = shared");
+            connection.Open();
+
+            _dbContextOptBuilder = new DbContextOptionsBuilder<DatabaseContext>().UseSqlite(connection);
+            _dbContext = new DatabaseContext(_dbContextOptBuilder.Options);
+
+            DatabaseInitializer.ContextSeederAsync(_dbContext).Wait();
+        }
+
+        protected DatabaseContext DbContext => new DatabaseContext(_dbContextOptBuilder.Options);
+    }
+}
