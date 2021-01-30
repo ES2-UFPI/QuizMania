5369c1dae047d8d61a0b65f4938457dfa367d088
Daniel Dantas <daniel.leite.dantas@gmail.com>
2021-01-30 11:09:39 -0300

    Implemented basic tests for GetCharacter API #87

---
+++ QuizMania.WebAPI.Tests/Controllers/CharacterController_Tests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuizMania.WebAPI.Controllers;
+using QuizMania.WebAPI.Services;
+using QuizMania.WebAPI.DTOs.Output;
+
+namespace QuizMania.WebAPI.Tests.Controllers
+{
+    [TestFixture]
+    class CharacterController_Tests : DbContextTestBase
+    {
+        [TestCase(-1)]
+        [TestCase(0)]
+        public async Task TestGetCharacterInvalidId(long value)
+        {
+            var controller = new CharacterController(new CharacterService(new CharacterRepository(DbContext), new ItemRepository(DbContext), Mapper));
+            var actionResult = await controller.GetCharacter(value);
+
+            Assert.IsInstanceOf<NotFoundResult>(actionResult);
+        }
+
+        [TestCase(999)]
+        public async Task TestGetCharacterNonExistent(long value)
+        {
+            var controller = new CharacterController(new CharacterService(new CharacterRepository(DbContext), new ItemRepository(DbContext), Mapper));
+            var actionResult = await controller.GetCharacter(value);
+
+            Assert.IsInstanceOf<NotFoundResult>(actionResult);
+        }
+
+        [TestCase(1)]
+        public async Task TestGetCharacterExistent(long value)
+        {
+            var controller   = new CharacterController(new CharacterService(new CharacterRepository(DbContext), new ItemRepository(DbContext), Mapper));
+            var actionResult = await controller.GetCharacter(value);
+            
+            var okResult = actionResult as OkObjectResult;
+            Assert.NotNull(okResult);
+            
+            var characterInfo = okResult.Value as CharacterInfoDTO;
+            Assert.NotNull(characterInfo);
+            
+            Assert.IsTrue(characterInfo.Id == value);
+        }   
+    }
+}
